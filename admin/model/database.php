0080f62014aad5a588af1eafe6dd78660db03f01
nczimmerman00 <89539108+nczimmerman00@users.noreply.github.com>
2022-03-25 21:11:39 -0500

    Update database.php

--- admin/model/database.php
+++ admin/model/database.php
@@ -1,13 +1,13 @@
 <?php 
-//$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-//$username = 'mgs_user';
-//$password = 'pa55word';
-$dsn = "mysql:host=localhost; port=3307; dbname=zippyusedautos";
-$username = 'root';
+$dsn = 'mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=a83ggl9p6k1088o0';
+$username = 'orccuw7hr3h0lbua';
+$password = 'mior2tc64286j19j';
+//$dsn = "mysql:host=localhost; port=3307; dbname=zippyusedautos";
+//$username = 'root';
 
 try{
-    $db = new PDO($dsn, $username);
-    //$db = new PDO($dsn, $username, $password);
+    //$db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 }catch (PDOException $e){
     $error = 'Database Error: ';
     $error .= $e->getMessage();
